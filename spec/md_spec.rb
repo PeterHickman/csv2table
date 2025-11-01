f33b8390c171d14b457e5c5a08d59940d4268cfb
Peter Hickman <peterhickman386@googlemail.com>
2025-11-01 13:42:25 +0000

    cleanup

--- spec/md_spec.rb
+++ spec/md_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'convert csv data' do
+describe 'markdown output' do
   before :all do
     build_program
   end
@@ -13,56 +13,54 @@ describe 'convert csv data' do
     remove_file('output.txt')
   end
 
-  context 'markdown output' do
-    context 'from stdio' do
-      context 'with the defaults' do
-        it 'creates the table' do
-          exec('cat ./spec/data.csv | ./csv2table --md > ./output.txt')
+  context 'from stdio' do
+    context 'with the defaults' do
+      it 'creates the markdown' do
+        exec('cat ./spec/data.csv | ./csv2table --md > ./output.txt')
 
-          contents_the_same('data.md')
-        end
-      end
-
-      context 'delimiter is ,' do
-        it 'creates the table' do
-          exec('cat ./spec/data.csv | ./csv2table --md -delimit , > ./output.txt')
-
-          contents_the_same('data.md')
-        end
-      end
-
-      context 'delimiter is tab' do
-        it 'creates the table' do
-          exec('cat ./spec/data.tsv | ./csv2table --md -delimit "\t" > ./output.txt')
-
-          contents_the_same('data.md')
-        end
+        contents_the_same('data.md')
       end
     end
 
-    context 'as argument' do
-      context 'with the defaults' do
-        it 'creates the table' do
-          exec('./csv2table --md ./spec/data.csv > ./output.txt')
+    context 'delimiter is ,' do
+      it 'creates the markdown' do
+        exec('cat ./spec/data.csv | ./csv2table --md -delimit , > ./output.txt')
 
-          contents_the_same('data.md')
-        end
+        contents_the_same('data.md')
       end
+    end
 
-      context 'delimiter is ,' do
-        it 'creates the table' do
-          exec('./csv2table -delimit , --md ./spec/data.csv > ./output.txt')
+    context 'delimiter is tab' do
+      it 'creates the markdown' do
+        exec('cat ./spec/data.tsv | ./csv2table --md -delimit "\t" > ./output.txt')
 
-          contents_the_same('data.md')
-        end
+        contents_the_same('data.md')
       end
+    end
+  end
 
-      context 'delimiter is tab' do
-        it 'creates the table' do
-          exec('./csv2table -delimit "\t" --md ./spec/data.tsv > ./output.txt')
+  context 'as argument' do
+    context 'with the defaults' do
+      it 'creates the markdown' do
+        exec('./csv2table --md ./spec/data.csv > ./output.txt')
 
-          contents_the_same('data.md')
-        end
+        contents_the_same('data.md')
+      end
+    end
+
+    context 'delimiter is ,' do
+      it 'creates the markdown' do
+        exec('./csv2table -delimit , --md ./spec/data.csv > ./output.txt')
+
+        contents_the_same('data.md')
+      end
+    end
+
+    context 'delimiter is tab' do
+      it 'creates the markdown' do
+        exec('./csv2table -delimit "\t" --md ./spec/data.tsv > ./output.txt')
+
+        contents_the_same('data.md')
       end
     end
   end
--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'convert csv data' do
+describe 'table output' do
   before :all do
     build_program
   end
@@ -12,57 +12,54 @@ describe 'convert csv data' do
   after :each do
     remove_file('output.txt')
   end
+  context 'from stdio' do
+    context 'with the defaults' do
+      it 'creates the table' do
+        exec('cat ./spec/data.csv | ./csv2table > ./output.txt')
 
-  context 'table output' do
-    context 'from stdio' do
-      context 'with the defaults' do
-        it 'creates the table' do
-          exec('cat ./spec/data.csv | ./csv2table > ./output.txt')
-
-          contents_the_same('data.txt')
-        end
-      end
-
-      context 'delimiter is ,' do
-        it 'creates the table' do
-          exec('cat ./spec/data.csv | ./csv2table -delimit , > ./output.txt')
-
-          contents_the_same('data.txt')
-        end
-      end
-
-      context 'delimiter is tab' do
-        it 'creates the table' do
-          exec('cat ./spec/data.tsv | ./csv2table -delimit "\t" > ./output.txt')
-
-          contents_the_same('data.txt')
-        end
+        contents_the_same('data.txt')
       end
     end
 
-    context 'as argument' do
-      context 'with the defaults' do
-        it 'creates the table' do
-          exec('./csv2table ./spec/data.csv > ./output.txt')
+    context 'delimiter is ,' do
+      it 'creates the table' do
+        exec('cat ./spec/data.csv | ./csv2table -delimit , > ./output.txt')
 
-          contents_the_same('data.txt')
-        end
+        contents_the_same('data.txt')
       end
+    end
 
-      context 'delimiter is ,' do
-        it 'creates the table' do
-          exec('./csv2table -delimit , ./spec/data.csv > ./output.txt')
+    context 'delimiter is tab' do
+      it 'creates the table' do
+        exec('cat ./spec/data.tsv | ./csv2table -delimit "\t" > ./output.txt')
 
-          contents_the_same('data.txt')
-        end
+        contents_the_same('data.txt')
       end
+    end
+  end
 
-      context 'delimiter is tab' do
-        it 'creates the table' do
-          exec('./csv2table -delimit "\t" ./spec/data.tsv > ./output.txt')
+  context 'as argument' do
+    context 'with the defaults' do
+      it 'creates the table' do
+        exec('./csv2table ./spec/data.csv > ./output.txt')
 
-          contents_the_same('data.txt')
-        end
+        contents_the_same('data.txt')
+      end
+    end
+
+    context 'delimiter is ,' do
+      it 'creates the table' do
+        exec('./csv2table -delimit , ./spec/data.csv > ./output.txt')
+
+        contents_the_same('data.txt')
+      end
+    end
+
+    context 'delimiter is tab' do
+      it 'creates the table' do
+        exec('./csv2table -delimit "\t" ./spec/data.tsv > ./output.txt')
+
+        contents_the_same('data.txt')
       end
     end
   end
